45f75067561ff98aa5ab40aa473affddf915beef
PetaJanata <241151529+PetaJanata@users.noreply.github.com>
2025-11-04 12:53:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const videos = [
   { src: "videos/9_480.mp4" },
   { src: "videos/10_480.mp4" },
   { src: "videos/15_480.mp4" },
-  { src: "videos/figure1.mp4" }
+
 ];
 
 const gallery = document.getElementById("video-gallery");
